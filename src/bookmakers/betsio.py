2b12092784c45104ef0994010cc3f62e39a11ca9
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-09-07 12:00:18 +0100

    add betsio

---
+++ src/bookmakers/betsio.py
@@ -0,0 +1,76 @@
+from bs4 import BeautifulSoup
+
+# import requests
+import json
+import aiohttp
+
+# https://api.kineko.com/events?time=all&leagues%5B%5D=206230140992253952
+
+# first
+# https://sb2frontend-altenar2.biahosted.com/api/Sportsbook/GetEvents?timezoneOffset=-60&langId=8&skinName=stake&configId=12&culture=en-GB&countryCode=GB&deviceType=Desktop&numformat=en&integration=stake&sportids=66&categoryids=0&champids=2936&group=AllEvents&period=periodall&withLive=false&outrightsDisplay=none&marketTypeIds=&couponType=0&marketGroupId=0&startDate=2023-09-05T16%3A20%3A00.000Z&endDate=2023-09-12T16%3A20%3A00.000Z
+# https://sb2frontend-altenar2.biahosted.com/api/Sportsbook/GetEvents?timezoneOffset=-60&langId=8&skinName=stake&configId=12&culture=en-GB&countryCode=GB&deviceType=Desktop&numformat=en&integration=stake&sportids=66&categoryids=0&champids=2936&group=AllEvents&period=periodall&withLive=false&outrightsDisplay=none&marketTypeIds=&couponType=0&marketGroupId=0&startDate=2023-09-05T16:20:00.000Z&endDate=2023-09-12T16:20:00.000Z
+competition_urls = {
+    "football": {
+        "bundesliga": "https://sport.bets.io/api/v2/matches?bettable=true&limit=100&match_status=0&sort_by=tournament.priority%3Aasc&sort_by=tournament.id%3Aasc&sort_by=start_time%3Aasc&sort_by=bets_count%3Adesc&tournament_id=15&type=match",
+        "ligue1": "https://sport.bets.io/api/v2/matches?bettable=true&limit=100&match_status=0&sort_by=tournament.priority%3Aasc&sort_by=tournament.id%3Aasc&sort_by=start_time%3Aasc&sort_by=bets_count%3Adesc&tournament_id=18&type=match",
+        "liga": "https://sport.bets.io/api/v2/matches?bettable=true&limit=100&match_status=0&sort_by=tournament.priority%3Aasc&sort_by=tournament.id%3Aasc&sort_by=start_time%3Aasc&sort_by=bets_count%3Adesc&tournament_id=17&type=match",
+        "premier-league": "https://sport.bets.io/api/v2/matches?bettable=true&limit=100&match_status=0&sort_by=tournament.priority%3Aasc&sort_by=tournament.id%3Aasc&sort_by=start_time%3Aasc&sort_by=bets_count%3Adesc&tournament_id=14&type=match",
+        "serie-a": "https://sport.bets.io/api/v2/matches?bettable=true&limit=100&match_status=0&sort_by=tournament.priority%3Aasc&sort_by=tournament.id%3Aasc&sort_by=start_time%3Aasc&sort_by=bets_count%3Adesc&tournament_id=16&type=match",
+        "primeira": "https://sport.bets.io/api/v2/matches?bettable=true&limit=100&match_status=0&sort_by=tournament.priority%3Aasc&sort_by=tournament.id%3Aasc&sort_by=start_time%3Aasc&sort_by=bets_count%3Adesc&tournament_id=51&type=match",
+        "serie-a-brasil": "https://sport.bets.io/api/v2/matches?bettable=true&limit=100&match_status=0&sort_by=tournament.priority%3Aasc&sort_by=tournament.id%3Aasc&sort_by=start_time%3Aasc&sort_by=bets_count%3Adesc&tournament_id=178&type=match",
+        "bundesliga-austria": "https://sport.bets.io/api/v2/matches?bettable=true&limit=100&match_status=0&sort_by=tournament.priority%3Aasc&sort_by=tournament.id%3Aasc&sort_by=start_time%3Aasc&sort_by=bets_count%3Adesc&tournament_id=177&type=match",
+        "division-1a": "https://sport.bets.io/api/v2/matches?bettable=true&limit=100&match_status=0&sort_by=tournament.priority%3Aasc&sort_by=tournament.id%3Aasc&sort_by=start_time%3Aasc&sort_by=bets_count%3Adesc&tournament_id=176&type=match",
+    },
+    "basketball": {
+        "nba": "https://sport.bets.io/api/v2/matches?bettable=true&limit=100&match_status=0&sort_by=tournament.priority%3Aasc&sort_by=tournament.id%3Aasc&sort_by=start_time%3Aasc&sort_by=bets_count%3Adesc&tournament_id=12&type=match",
+        # "euroleague": "https://www.betclic.fr/basket-ball-s4/euroligue-c14",
+    },
+    "tennis": {
+        # "us-open-men": get_url(3027),
+    },
+}
+
+
+async def get_page(competition):
+    async with aiohttp.ClientSession() as session:
+        if (
+            competition["sport"] in competition_urls
+            and competition["competition"] in competition_urls[competition["sport"]]
+        ):
+            url = competition_urls[competition["sport"]][competition["competition"]]
+        else:
+            return None
+        async with session.get(
+            url,
+            headers={
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+            },
+        ) as response:
+            response = await response.json()
+        return response
+
+
+async def get_games(competition):
+    response = await get_page(competition)
+    result = response["data"]
+    games = []
+    for el in result:
+        team1 = el["competitors"]["home"]["name"]
+        team2 = el["competitors"]["away"]["name"]
+        first = None
+        second = None
+        third = None
+        market = el["main_market"]
+        outcomes = market["outcomes"]
+        for outcome in outcomes:
+            if outcome["outcome_external_id"] == "1":
+                first = outcome["odds"] / 1000
+            if outcome["outcome_external_id"] == "2":
+                second = outcome["odds"] / 1000
+            if outcome["outcome_external_id"] == "3":
+                third = outcome["odds"] / 1000
+
+        if team1 and team2 and first and second and third:
+            odds = [float(first), float(second), float(third)]
+            games.append({"team1": team1, "team2": team2, "odds": odds})
+    return games
--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ import bookmakers.sportsbetio as sportsbetio
 import bookmakers.kineko as kineko
 import bookmakers.chipstars as chipstars
 import bookmakers.vave3 as vave3
+import bookmakers.betsio as betsio
 import arb
 import sys
 import log
@@ -54,13 +55,18 @@ async def check_competition(competition):
     print("Checking competition: {}".format(competition))
     results = await asyncio.gather(
         get_competition_games("kineko", kineko, competition),
-        # get_competition_games("sportsbetio", sportsbetio, competition),  exactly the same as stake
+        # get_competition_games(
+        #     "sportsbetio", sportsbetio, competition
+        # ),  # exactly the same as stake
         get_competition_games("trustdice", trustdice, competition),
         get_competition_games("betplay", betplay, competition),
-        # get_competition_games("jackbit", jackbit, competition), exactly the same as freshbet
+        # get_competition_games(
+        #     "jackbit", jackbit, competition
+        # ),  # exactly the same as freshbet
         get_competition_games("freshbet", freshbet, competition),
         get_competition_games("stake", stake, competition),
         get_competition_games("vave3", vave3, competition),
+        get_competition_games("betsio", betsio, competition),
         # get_competition_games("netbet", netbet, competition),
         # get_competition_games("zebet", zebet, competition),
         # get_competition_games("chipstars", chipstars, competition), #requires selenium
